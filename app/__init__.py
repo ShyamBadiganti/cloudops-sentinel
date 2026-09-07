4c9bfdc63d0761c1a61ad374aa68c66ac0c71eb0
Shyam Badiganti <badigantishyam@outlook.com>
2026-09-07 22:24:59 +0530

    Initial project setup

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+def create_app():
+    app = Flask(__name__)
+    @app.route("/")
+    def home():
+        return "CloudOps Sentinel is running!"
+    return app
